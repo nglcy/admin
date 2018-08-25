ea397faef5defbd3eb275b3b7b8b450ecfec9306
nglcy <497390403@qq.com>
2018-08-25 10:50:19 +0800

    Initial commit

---
+++ src/main/java/Stu.java
@@ -0,0 +1,21 @@
+/**
+    * @Title: Stu
+    * @Description: (用一句话描述该文件做什么)
+    * @author chenyuan
+    * @date  2018/8/25 10:05
+    * @version V1.0
+    */
+public class Stu {
+
+    private int s ;
+    static{
+        int ss = 11;
+
+        System.out.print(ss);
+    }
+    public  static void main(String[] args){
+        new Stu();
+    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!12313131</h2>
+</body>
+</html>
